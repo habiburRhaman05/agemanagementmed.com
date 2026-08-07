2c7f62f37b515b1242d11cfeda5a184e7f1e3e14
repon001 <repon7253@gmail.com>
2026-08-07 23:38:25 +0600

    fix button

--- src/components/shared/BookAppointmentButton.tsx
+++ src/components/shared/BookAppointmentButton.tsx
@@ -56,7 +56,7 @@ export default function BookAppointmentButton({
         )}
       >
         {children}
-     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="12" viewBox="0 0 22 12" fill="none">
+     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="12" viewBox="0 0 22 12" fill="none" className="transition-transform duration-300 ease-out group-hover:translate-x-2">
 <path d="M1 5.6059C0.585786 5.6059 0.25 5.94168 0.25 6.3559C0.25 6.77011 0.585786 7.1059 1 7.1059V5.6059ZM21.5303 6.88623C21.8232 6.59333 21.8232 6.11846 21.5303 5.82557L16.7574 1.0526C16.4645 0.759702 15.9896 0.759702 15.6967 1.0526C15.4038 1.34549 15.4038 1.82036 15.6967 2.11326L19.9393 6.3559L15.6967 10.5985C15.4038 10.8914 15.4038 11.3663 15.6967 11.6592C15.9896 11.9521 16.4645 11.9521 16.7574 11.6592L21.5303 6.88623ZM1 7.1059H21V5.6059H1V7.1059Z" fill="white"/>
 </svg>
       </button>
