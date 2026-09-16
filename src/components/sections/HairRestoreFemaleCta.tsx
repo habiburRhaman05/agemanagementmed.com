65a98609996b0c1e03c98087dd2fb90155f94d56
habiburRhaman05 <devhabib2005@gmail.com>
2026-09-16 07:55:01 +0600

    fix btn issues

--- src/components/sections/HairRestoreFemaleCta.tsx
+++ src/components/sections/HairRestoreFemaleCta.tsx
@@ -3,7 +3,6 @@ import type { CSSProperties } from 'react'
 
 import { homeMedia } from '@/content/pages/home-media'
 import type { ClosingCtaData } from '@/types/content'
-import BookAppointmentButton from '../shared/BookAppointmentButton'
 
 interface ClosingCTAProps extends ClosingCtaData {
   /** Background photo for the band; defaults to the shared placeholder. */
@@ -58,13 +57,9 @@ export function HairRestoreFemaleCta({
               className="cta flex justify-center lg:justify-start"
               style={centered ? { display: 'flex', justifyContent: 'center' } : undefined}
             >
-              {/* <LegacyCtaLink href={cta.href} className="lg-btn ">
+              <LegacyCtaLink href={cta.href} className="lg-btn ">
                 {cta.label}
-              </LegacyCtaLink> */}
-              <BookAppointmentButton>
-                {cta.label}
-
-              </BookAppointmentButton>
+              </LegacyCtaLink>
             </div>
 
             {note ? (
